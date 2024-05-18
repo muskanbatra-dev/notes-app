6ae7063f5077af2922385ad8c7f508a09bfd3fbe
muskanbatra-dev <95659002+muskanbatra-dev@users.noreply.github.com>
2024-05-18 19:10:24 +0530

    initial commit

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,52 @@
-//importing packages and middlewares
+//importing packges and middlewares;
 const express = require("express");
-// const morgan = require("morgan");
-// const helmet = require("helmet");
-// const mongoose = require("mongoose");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const mongoose = require("mongoose");
 
-// creating app
+//configuring env files
+require("dotenv").config();
+
+//importing created routers;
+
+const authRouter = require("./routes/auth");
+const notesRouter = require("./routes/notes");
+
+//creating app
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Helloword");
-});
-//listening app on port
-app.listen(3000, () => {
-  console.log("server is running");
+//adding middlewares
+app.use(express.json());
+app.use(morgan("common"));
+app.use(helmet());
+
+//mongoDB connection
+const connectToDB = () => {
+  try {
+    const DB_Url = process.env.MONGODB_URL;
+    mongoose
+      .connect(DB_Url, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      })
+      .then(() => {
+        console.log("mongo db database connected");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  } catch (error) {
+    console.log("something went wrong...");
+  }
+};
+//util function to make connection with DB
+connectToDB();
+
+//normal API;
+
+app.use("/auth", authRouter);
+app.use("/notes", notesRouter);
+//listing app on port 8000
+app.listen(9000, () => {
+  console.log("server is running on localhost:8000");
 });
